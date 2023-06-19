3dbd6d16e246b110cff7a08ec4bdd2170843c73d
i.etga <akayetga@gmail.com>
2023-06-19 13:32:58 +0200

    review week3 created

---
+++ src/test/java/src/cydeo/tests/review/week3/TC3_iFrame_practice.java
@@ -0,0 +1,73 @@
+package src.cydeo.tests.review.week3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import src.cydeo.tests.base.TestBase;
+import src.cydeo.utilities.ConfigurationReader;
+import src.cydeo.utilities.Driver;
+
+public class TC3_iFrame_practice extends TestBase {
+
+    //// TC : Iframe practice
+
+    @BeforeMethod
+    public void setupMethod() {
+
+        //2. Go to: https://login1.nextbasecrm.com/
+        String URL = ConfigurationReader.getProperty("crmURL");
+        Driver.getDriver().get(URL);
+
+        //// 1. Users are already login and on the homepage of CRM app
+
+        //Enter valid username
+        WebElement inputUsername = Driver.getDriver().findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk45@cybertekschool.com");
+
+        //Enter valid password
+        WebElement inputPassword = Driver.getDriver().findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //Click to`LogIn`button
+        WebElement loginButton = Driver.getDriver().findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+    }
+
+    @Test
+    public void test1_crm_iFrame() {
+
+
+        //create locator
+        //switch to iFrame
+
+        Driver.getDriver().findElement(By.xpath("//span[.='Message']")).click();
+        Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-idPostFormLHE_blogPostForm\"]/iframe")));
+        WebElement messageTabIframe = Driver.getDriver().findElement(By.xpath("//body[@ contenteditable ='true']"));
+
+        //Users write test message
+        messageTabIframe.sendKeys("Hello Guys!");
+
+        // back to parent/default frame
+        Driver.getDriver().switchTo().parentFrame();
+
+        // 4. Users click the SEND button
+        WebElement sendButton = Driver.getDriver().findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendButton.click();
+
+        // 5. Verify the message is displayed on the feed
+        WebElement blogMessage = Driver.getDriver().findElement(By.xpath("//div[contains(@id,'blog_post_body')]"));
+
+        String expectedText = "Hello Guys!";
+        String actualText = blogMessage.getText();
+
+        Assert.assertTrue(blogMessage.isDisplayed());
+        Assert.assertEquals(actualText, expectedText);
+
+    }
+
+}
+
+
---
+++ src/test/java/src/cydeo/tests/review/week3/TC5_WebTable_practice.java
@@ -0,0 +1,81 @@
+package src.cydeo.tests.review.week3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import src.cydeo.tests.base.TestBase;
+import src.cydeo.utilities.ConfigurationReader;
+import src.cydeo.utilities.Driver;
+
+import java.util.List;
+
+public class TC5_WebTable_practice extends TestBase{
+
+
+    @Test
+    public void webTables_test1() {
+
+        // 2. Go to https://practice.cydeo.com/tables
+        String URL = ConfigurationReader.getProperty("practiceWebTablesURL");
+        Driver.getDriver().get(URL);
+
+        // 3. Print table data as a single String, not so common, but useful sometimes
+
+        // Find all tables on the page
+        List<WebElement> tables = Driver.getDriver().findElements(By.tagName("table"));
+
+        // Initialize an empty string to store the table data
+        String tableData = "";
+
+        // Iterate over each table
+        for (WebElement table : tables) {
+            // Find all rows in the table body
+            List<WebElement> rows = table.findElements(By.tagName("tr"));
+
+            // Iterate over each row
+            for (WebElement row : rows) {
+                // Find all cells in the row
+                List<WebElement> cells = row.findElements(By.tagName("td"));
+
+                // Iterate over each cell and extract the text
+                for (WebElement cell : cells) {
+                    if (cell.getText().contains("edit")) { // ignores edit delete !!
+                        continue;
+                    }
+                    tableData += cell.getText().trim() + " ";
+                }
+
+                tableData += "\n";  // Add a new line after each row
+            }
+        }
+        System.out.println(tableData);
+
+        // 4. verify tconway@earthlink.net is anywhere in the table
+        String expectedText = " tconway@earthlink.net";
+        Assert.assertTrue(tableData.contains(expectedText));
+
+        // 5. print all column names in single line
+        // Find all th on the page
+        List<WebElement> textHeaders = Driver.getDriver().findElements(By.tagName("th"));
+        String tHeaders = "";
+        String tHeadersLine ="";
+        for (WebElement each : textHeaders) {
+            String text = each.getText();
+            if(tHeaders.contains(text)){
+                continue;
+            }
+            tHeaders += text.trim() + "-";
+            tHeadersLine += text.trim() + "\n";
+        }
+        if(tHeaders.endsWith("-")) { // removes the last '-'
+            tHeaders = tHeaders.substring(0,tHeaders.length() - 1);
+        }
+
+        System.out.println(tHeaders);
+
+        // 6. print each column name in separate lines using a loop and findElements method
+        System.out.println(tHeadersLine);
+    }
+
+}
